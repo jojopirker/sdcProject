8532c958b7b2a16a9a0a40fdd8ec3b4efa7531d3
jojopirker <j.pirker@gmail.com>
2022-01-03 16:50:59 +0100

    frontend updates for iframe

--- frontend/sdc-frontend/src/App.js
+++ frontend/sdc-frontend/src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import DashIntegration from './components/DashIntegrationHack';
+import Navigation from './components/Navigation';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dash-1" element={
+            <DashIntegration route={"page-1"} />} />
+          <Route path="/dash-2" element={
+            <DashIntegration route={"page-2"} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
+
   );
 }
 
--- frontend/sdc-frontend/src/components/DashIntegrationHack.js
+++ frontend/sdc-frontend/src/components/DashIntegrationHack.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Spinner from 'react-bootstrap/Spinner';
 
 /**
  * DashIntegration is a hack to present an iframe for the Dash
- * @returns 
+ * @returns The iframe with the dash app
  */
-const DashIntegration = () => {
+export const DashIntegration = ({ route }) => {
+    const [loading, setLoading] = useState(true);
 
-    return(<>
-    <h2>Dashboard</h2>
-    Below is the Dashboard. Loading may take time...
-    <iframe src="" />
+    return (<>
+        <h2>Dashboard</h2>
+        {loading &&
+            <>Loading may take time...
+                <Spinner animation="border" />
+            </>}
+        <Container>
+            <iframe src={`http://127.0.0.1:8050/${route}`}
+                style={{ width: "100%", height: "75vh" }}
+                onLoad={() => { setLoading(false) }} /> {/**todo change url**/}
+        </Container>
+
     </>)
 }
+
+export default DashIntegration;

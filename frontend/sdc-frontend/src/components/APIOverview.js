9852333a6defd720823cba33e42abfc216b2567c
jojopirker <j.pirker@gmail.com>
2022-01-08 19:46:10 +0100

    Revert "Update frontend_build.yml"
    This reverts commit 968d1d23fa4640cfdf7429d4530b1d94aa7d8d9e.

--- frontend/sdc-frontend/src/components/APIOverview.js
+++ frontend/sdc-frontend/src/components/APIOverview.js
@@ -1,10 +1,14 @@
+import Container from 'react-bootstrap/Container'
+
 /**
  * 
  * @returns APIOverview
  */
 const APIOverview = () => {
     <>
-    <h1>API Overview</h1>
+        <Container>
+            <h1>API Overview</h1>
+        </Container>
     </>
 }
 

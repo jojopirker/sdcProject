e3598a697f778f28afd87e36a5297745223fccd8
Alexander B <alex.bruckner97@gmx.at>
2022-01-15 18:20:06 +0100

    loaded model in backend

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,44 @@
 from fastapi import FastAPI
 from typing import Optional
+import joblib
+import pandas as pd
+from pydantic import BaseModel
 
 #
 app = FastAPI()
+knn_model = None
+
+input_format_df = None
+
+class Item(BaseModel):
+    day_of_week: str
+    first_road_class: str
+    weather_conditions: str
+    road_surface_conditions: str
+    special_conditions_at_site: str
+    urban_or_rural_area: str
+    longitude: float
+    latitude: float
+    daytime: str
+    season: str
+    vehicle_type: str
 
 @app.on_event("startup")
-async def load_models():
-    return
+async def startup_event():
+    filename = './model/finalized_model.pkl'
+    knn_model = joblib.load(filename)
+
+    input_format_df = pd.read_csv('./input_format/input_format.csv')
+    input_format_df = input_format_df[0:0]
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/items/{item_id}")
-def ask_model(item_id:int):
-    return {"test"}
+@app.post("/items")
+def ask_model(item: Item):
+    result = knn_model.predict() 
+    return {"accident_severty":result}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
